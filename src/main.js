b030979f66fb2b092f4146b6c7d1b71dd59c43e3
TARGEToff <lockontargt@gmail.com>
2021-12-04 20:57:36 +0100

    sliced 10 movies from 250 available

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ document.querySelector('#app').innerHTML = `
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
 
+
+// DATA FROM 250 BEST MOVIES
 fetch("https://imdb-api.com/en/API/Top250Movies/k_yju1poqs/")
     .then(result => result.json())
-    .then(data => console.log(data.items[0]))
+    .then(({items}) => {                // <---- items destructurized from data
+        for (item of items.slice(10, 20)) {
+            console.log(item.title, item.year, item.id)
+        }
+    })
 
 
+
+// DATA FROM SINGLE MOVIE
 fetch("https://imdb-api.com/en/API/Title/k_yju1poqs/tt0068646")
     .then(result => result.json())
     .then(data => {

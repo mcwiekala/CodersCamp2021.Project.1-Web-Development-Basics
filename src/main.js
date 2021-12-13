1cf1750cf52ab2b0804c1c424f5cb6021b19eba9
TARGEToff <lockontargt@gmail.com>
2021-12-13 21:21:05 +0100

    changed for loop into forEach function

--- src/main.js
+++ src/main.js
@@ -7,14 +7,9 @@ const instance = axios.create({
     baseURL: 'http://www.omdbapi.com/',
   });
 
-
 const moviesId = ["tt0816692", "tt0103064", "tt1675434", "tt0910970", "tt4633694", "tt1675434", "tt0078748", "tt1446714", "tt3896198", "tt1160419"];
 
-const getMovies = async() => {
-    for(movieId of moviesId) {
-        const response = await instance.get(`?i=${movieId}&apikey=46e8c41c`);
-        console.log(response.data);
-    }
-}
-
-getMovies();
+moviesId.forEach(async movieId => {
+    const response = await instance.get(`?i=${movieId}&apikey=46e8c41c`);
+    console.log(response.data);
+})

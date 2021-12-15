78599b1b2d60ad2c0b6756edef46ccefbe89d4e2
TARGEToff <lockontargt@gmail.com>
2021-12-15 17:45:49 +0100

    moved apiKey into variable

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Header from './components/header/header';
 import Footer from "./components/footer/footer";
 import axios from "axios";
 
+const apiKey = "46e8c41c"
 const instance = axios.create({
     baseURL: 'http://www.omdbapi.com/',
   });
@@ -12,6 +13,6 @@ const instance = axios.create({
 const moviesId = ["tt0816692", "tt0103064", "tt1675434", "tt0910970", "tt4633694", "tt1675434", "tt0078748", "tt1446714", "tt3896198", "tt1160419"];
 
 moviesId.forEach(async movieId => {
-    const response = await instance.get(`?i=${movieId}&apikey=46e8c41c`);
+    const response = await instance.get(`?i=${movieId}&apikey=${apiKey}`);
     console.log(response.data);
 }) 

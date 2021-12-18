099477297c329caebe72f3e8c026d875e5e86480
Cwiekala Michal <cwiekala.michal@gmail.com>
2021-12-18 09:09:03 +0100

    rule for vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
 };

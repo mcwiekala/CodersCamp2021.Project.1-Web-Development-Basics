161a31873246a60e1b4b5af0f40b15eeb3ef0489
Magdalena Krzak <magda@magdalenakrzak.pl>
2022-01-01 19:03:59 +0100

    test one collection - get titles and runtimes

--- src/main.js
+++ src/main.js
@@ -58,3 +58,34 @@ const bestOf2021MoviesId = [
   'tt11271038',
   'tt8430054',
 ];
+
+const c1 = {
+  collectionName: 'Best of 2021',
+  collectionDescription: 'Najlepsze filmy, które trafiły do kin w 2021 roku.',
+  totalLengthInMinutes: 823,
+  movieTitles: ['Film1', 'Film2', 'Film3'],
+  bestOf2021MoviesId: [
+    'tt10370710',
+    'tt0067372',
+    'tt14039582',
+    'tt6817944',
+    'tt8633462',
+    'tt11271038',
+    'tt8430054',
+  ],
+};
+
+c1.bestOf2021MoviesId.forEach(async (movieId) => {
+  const {
+    data: { Runtime },
+  } = await instance.get(`?i=${movieId}&apikey=${apiKey}`);
+  console.log (Runtime)
+});
+
+
+c1.bestOf2021MoviesId.forEach(async (movieId) => {
+  const {
+    data: { Title },
+  } = await instance.get(`?i=${movieId}&apikey=${apiKey}`);
+  console.log (Title)
+});

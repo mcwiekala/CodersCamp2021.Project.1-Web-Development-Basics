0e501764df51f0810384c32d0e299709483c7205
Cwiekala Michal <cwiekala.michal@gmail.com>
2022-01-02 13:53:04 +0100

    Updates

--- assets/collectionDetails.6a7370f1.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill.b7f2da20.js";/* empty css              */import"./main.c08606a1.js";import"./vendor.a6cc7faa.js";
---
+++ assets/main.98e487ca.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.b7f2da20.js";/* empty css              */import"./main.88bd989f.js";import"./vendor.a6cc7faa.js";

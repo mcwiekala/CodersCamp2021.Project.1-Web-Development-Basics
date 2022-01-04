1f0b97630f3d9b3bfebd6d434353e1ca15a1e029
TARGEToff <lockontargt@gmail.com>
2022-01-04 21:35:29 +0100

    styling correction, fixed order of ratings

--- src/pages/movie-details/movie-details.js
+++ src/pages/movie-details/movie-details.js
@@ -13,12 +13,10 @@ const {
     data: { Title, Runtime, Year, Genre, Poster, Plot, Ratings },
   } = await instance.get(`?i=${sessionStorage.movieId}&apikey=${apiKey}`);
 
-console.log(Title, Runtime, Year, Genre)
-
 document.querySelector('.movie-detail__title').innerText = Title
 document.querySelector('.movie-detail__image').src = Poster
 document.querySelector('.movie-detail__info').innerText = `${Year} | ${Runtime} | ${Genre.split(',')[0]}`
 document.querySelector('.movie-detail__description').innerText = Plot
-document.querySelector('.movie-detail__metacritic-rate').innerText = Ratings[2].Value
+document.querySelector('.movie-detail__imdb-rate').innerText = Ratings[0].Value
 document.querySelector('.movie-detail__tomatoes-rate').innerText = Ratings[1].Value
-document.querySelector('.movie-detail__movie-db-rate').innerText = Ratings[0].Value
+document.querySelector('.movie-detail__metacritic-rate').innerText = Ratings[2].Value

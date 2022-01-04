33fab4fc6e234d381598f0c3bcbc001c3bc2ddf2
Cwiekala Michal <michal.cwiekala-extern@dbschenker.com>
2022-01-04 09:08:17 +0100

    copied view

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,12 @@ module.exports = defineConfig({
           'movie-details',
           'index.html',
         ),
+        collections: resolve(
+          root,
+          'pages',
+          'collections',
+          'index.html',
+        ),
       },
     },
   },

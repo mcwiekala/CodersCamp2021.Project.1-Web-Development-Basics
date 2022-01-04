7a63ddfc97e8b03c43309eee641fdda86c3611dc
dawidDwk <dawiddrzewinski@gmail.com>
2022-01-04 23:51:10 +0100

    collectuion-details time-fully-worked

--- src/pages/collection-details/collection-details.js
+++ src/pages/collection-details/collection-details.js
@@ -6,6 +6,37 @@ window.onload = function () {
   const params = new URL(window.location).searchParams;
   const idName = params.get('id');
   const localData = JSON.parse(localStorage.getItem(idName));
+  const moviesWatched = JSON.parse(localStorage.getItem('moviesWatched'));
+  const movies = JSON.parse(localStorage.getItem('Films'));
+  const moviesCollection = localData.movieids;
+  const filteredArray = moviesWatched.filter((value) =>
+    moviesCollection.includes(value)
+  );
+
+  const fimls = filteredArray
+    .map((id) => movies.find((film) => film.imdbID === id))
+    .filter((id) => id !== undefined);
+
+  let times = 0;
+
+  fimls.forEach((element) => {
+    const time = element.Runtime.split(' ')[0];
+    const parsTime = parseInt(time);
+    times += parsTime;
+  });
+
+  const timePercentages = parseInt(
+    (times * 100) / localData.totalLengthInMinutes,
+    10
+  );
+  console.log(
+    '🚀 ~ file: collection-details.js ~ line 29 ~ timePercentages',
+    timePercentages
+  );
+
+  console.log('🚀 ~ file: collection-details.js ~ line 21 ~ times', times);
+
+  console.log('🚀 ~ file: collection-details.js ~ line 19 ~ fimls', fimls);
 
   function imgImpSearch() {
     if (localData.collectionImage === 'imageBest2021') {
@@ -23,5 +54,9 @@ window.onload = function () {
   document.querySelector('.collection__progress_name').innerHTML =
     localData.collectionName;
   document.querySelector('.collection__image_src').src = imgImpSearch();
-  document.querySelector('.collection__list_pos').innerHTML = imgImpSearch();
+  document.querySelector(
+    '.collection__progress_text'
+  ).innerHTML = `Watching time: ${parseInt(times / 60, 10)} hours ${
+    times % 60
+  } minutes`;
 };

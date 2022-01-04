0aff41082fc2cb525822bf699686e91b29471347
Judyta Baca <bacajudyta@gmail.com>
2022-01-04 09:50:31 +0100

    collections vs collection-details

--- src/components/collection-component/collection-component.js
+++ src/components/collection-component/collection-component.js
@@ -4,9 +4,9 @@ template.innerHTML = `
       @import "../../style.css"
   </style>
   <div class="section collection-section">
-    <a href="#"><img class="collection-poster" src="../../images/Avengers_Endgame_poster_035_Textless.jpg" /></a>
+    <a id="link1" href="../../pages/collection-details/index.html"><img class="collection-poster" src="../../images/Avengers_Endgame_poster_035_Textless.jpg" /></a>
     <div class="collection-details">
-        <a href="#"><h1 class="collection-title">SOME TITLE</h1></a>
+        <a id="link2" href="../../pages/collection-details/index.html"><h1 class="collection-title">SOME TITLE</h1></a>
         <h1 class="collection-description">Whatever description for collection</h1>
     </div>
   </div>
@@ -23,6 +23,8 @@ class CollectionComponent extends HTMLElement {
     this.shadowRoot.querySelector('.collection-title').innerHTML = this.getAttribute('collection-name')
     this.shadowRoot.querySelector('.collection-description').innerHTML = this.getAttribute('collection-text')
     this.shadowRoot.querySelector('.collection-poster').src = this.getAttribute('img-source')
+    this.shadowRoot.querySelector('#link1').href = this.getAttribute('collection-link')
+    this.shadowRoot.querySelector('#link2').href = this.getAttribute('collection-link')
   }
 }
 
